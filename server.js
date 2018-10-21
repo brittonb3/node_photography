007e8d33b765336ad24b66cccfb74855034a16ac
Britton Baumann <brittonbaumann@gmail.com>
2018-10-20 20:24:57 -0500

    host files

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // router our app
-const router = require('./app.js');
+const router = require('./public/app.js');
 app.use('/', router);
 
 // set static files (css and images, ect) location
