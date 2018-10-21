de9c1c2ec2da910d8ca6206b3eda7c632753da7f
Britton Baumann <brittonbaumann@gmail.com>
2018-10-20 21:07:53 -0500

    file host

--- public/app.js
+++ public/app.js
@@ -44,48 +44,48 @@ router.get('/contact_sent',function(req, res){
   res.render('pages/contact_sent');
 })
 
-// router.post('/contact', (req, res) => {
-//   const output = `
-//     <p>You have a new contact request</p>
-//     <h3>Contact details</h3>
-//     <ul>
-//       <li>Name: ${req.body.name}</li>
-//       <li>Email: ${req.body.email}</li>
-//     </ul>
-//     <h3>Message</h3>
-//     <p>${req.body.message}</p>
-//   `;
-//
-//   let transporter = nodemailer.createTransport({
-//         host: 'smtp.dreamhost.com',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: process.env.mailer_user, // generated ethereal user
-//             pass: process.env.mailer_pass// generated ethereal password
-//         },
-//         tls:{
-//           rejectUnauthorized:false
-//         }
-//     });
-//
-//     // setup email data with unicode symbols
-//     let mailOptions = {
-//         from: '"greatbritton" <britton@greatbrittonphotography.com>', // sender address
-//         to: 'britton@greatbrittonphotography.com', // list of receivers
-//         subject: 'new contact', // Subject line
-//         text: 'new message', // plain text body
-//         html: output // html body
-//     };
-//
-//     // send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message sent: %s', info.messageId);
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//
-//         res.render('pages/contact_sent', )
-//     });
-// });
+router.post('/contact', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+        host: 'smtp.dreamhost.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.mailer_user, // generated ethereal user
+            pass: process.env.mailer_pass// generated ethereal password
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"greatbritton" <britton@greatbrittonphotography.com>', // sender address
+        to: 'britton@greatbrittonphotography.com', // list of receivers
+        subject: 'new contact', // Subject line
+        text: 'new message', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('pages/contact_sent', )
+    });
+});
