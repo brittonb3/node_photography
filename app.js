3fa7e4009a12e20b085d95e602584106eae8eca4
Britton Baumann <brittonbaumann@gmail.com>
2018-10-25 16:14:56 -0500

    fixes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require express
-const express = require('express');
 const dotenv = require('dotenv').config()
+const express = require('express');
+
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const path = require('path');
@@ -44,52 +45,43 @@ router.get('/contact_sent',function(req, res){
   res.render('pages/contact_sent');
 })
 
-router.post('/contact', (req, res) => {
-  console.log(req.body);
-});
-
-router.post('/contact', (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact details</h3>
-    <ul>
-      <li>Name: ${req.body.name}</li>
-      <li>Email: ${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
-
-  let transporter = nodemailer.createTransport({
-        host: 'smtp.dreamhost.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.mailer_user, // generated ethereal user
-            pass: process.env.mailer_pass// generated ethereal password
-        },
-        tls:{
-          rejectUnauthorized:false
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"greatbritton" <britton@greatbrittonphotography.com>', // sender address
-        to: 'britton@greatbrittonphotography.com', // list of receivers
-        subject: 'new contact', // Subject line
-        text: 'new message', // plain text body
-        html: output // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        res.render('pages/contact_sent', )
-    });
-});
+// router.post('/contact', function(req, res){
+//   const output = {
+//     Name: '<li>Name: ${req.body.name}</li>',
+//     Email: '<li>Email: ${req.body.email}</li>',
+//     Message: '<p>${req.body.message}</p>'
+//   };
+//
+//   let transporter = nodemailer.createTransport({
+//         host: 'smtp.dreamhost.com',
+//         port: 587,
+//         secure: false, // true for 465, false for other ports
+//         auth: {
+//             user: process.env.mailer_user, // generated ethereal user
+//             pass: process.env.mailer_pass// generated ethereal password
+//         },
+//         tls:{
+//           rejectUnauthorized:false
+//         }
+//     });
+//
+//     // setup email data with unicode symbols
+//     let mailOptions = {
+//         from: '"greatbritton" <britton@greatbrittonphotography.com>', // sender address
+//         to: 'britton@greatbrittonphotography.com', // list of receivers
+//         subject: 'new contact', // Subject line
+//         text: 'new message', // plain text body
+//         html: output // html body
+//     };
+//
+//     // send mail with defined transport object
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log('Message sent: %s', info.messageId);
+//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//
+//         res.render('pages/contact_sent', )
+//     });
+// });
